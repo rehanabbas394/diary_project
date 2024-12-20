3ad3cd2284ac875aa70c945bdc26b5cb628d3aae
Rehan Abbas <136816198+rehanabbas394@users.noreply.github.com>
2024-12-20 16:20:38 +0500

    Update models.py

--- diary/models.py
+++ diary/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class DiaryEntry(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True) // keep user null false
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
